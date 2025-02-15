269f8f4d451d8996e7e97c9d35953b28f2e34aa1
D8ger <xyb5to0zay@gmail.com>
2025-02-15 13:51:05 +0800

    style(page):reduce the spacing

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-start bg-blue-50 py-4">
       <div className="container mx-auto px-4 flex flex-col items-center relative">
-        <div className="w-full flex justify-center min-h-[600px] mb-4">
+        <div className="w-full flex justify-center min-h-[600px] mb-1">
           <ClientBreathingWrapper />
         </div>
         <div className="w-full flex justify-center opacity-0 transition-opacity duration-500" 
